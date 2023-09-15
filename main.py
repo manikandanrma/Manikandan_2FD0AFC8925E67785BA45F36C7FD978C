e2a50d7aed9c59cad3d6d4b05d824bf8f00289dc
manikandanrma <144419599+manikandanrma@users.noreply.github.com>
2023-09-15 22:30:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
+class bankAccount:
 
-# Example usage:
-year = 2024
-if is_leap_year(year):
-    print(f"{year} is a leap year")
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    
+  self.__account_number = account_number
+  self.__account_holder_name = account_holder_name
+  self.__account_balance = initial_balance
+
+def deposit(self, amount):
+  if amount > 0:
+    self.__account_balance += amount
+    print("deposited ${}. New balance: ${}".format(amount,self.__account_balance))
+
 else:
-    print(f"{year} is not a leap year")
+  print("invalid deposit amount. Please deposit a positive amount.")
+
+def withdraw(self, amount):
+  if amount > 0 and amount <= self.__account_balance:
+    self.__account_balance -= amount
+    print("withdraw ${}. New balance: ${}".format(amount,self.__account_balance))
+
+else:
+print("Invalid withdrawal amount or insufficient balance.")
+
+def display_balance(self):
+  print("Account balance for{} (Account {}): ${}".format(
+    self.__account_holder_name, self.__account_number,
+    self.__account_balance))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
